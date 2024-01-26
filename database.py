e491138692b4372dc39ff7ad419616e07d5b798e
Ramprasad Reddy <ram@Ramprasads-MacBook-Pro.local>
2024-01-26 19:34:55 +0530

    Prod db- Postgresql added

--- database.py
+++ database.py
@@ -2,10 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+"""SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)"""
+
+SQLALCHEMY_DATABASE_URL = "postgresql://liozjuie:MWUe46F5YMwlLKt8_3b3Gt5vJuQXN5cC@rosie.db.elephantsql.com/liozjuie"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
